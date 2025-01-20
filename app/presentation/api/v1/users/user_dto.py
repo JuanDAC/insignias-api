8334cd4f2fcfd488025249941258074e45aaddf9
JuanDAC <juanavila12.0308@gmail.com>
2025-01-19 23:02:01 -0500

    :recycle: Refactor semantic regex

--- app/presentation/api/v1/users/user_dto.py
+++ app/presentation/api/v1/users/user_dto.py
@@ -2,6 +2,9 @@ from app.core.entities.user.user_entity import User as UserEntity
 from pydantic import BaseModel, EmailStr, field_validator, Field
 from re import match
 
+USERNAME_ALPHANUMERIC_REGEX = r"^[a-zA-Z0-9_]+$"
+PASSWORD_STRONG_WITH_SPECIAL_CHARS_REGEX = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+
 class UserCreate(BaseModel, UserEntity):
   id = Field(max_length=255)
   username = Field(max_length=255)
@@ -11,14 +14,14 @@ class UserCreate(BaseModel, UserEntity):
   @field_validator('username', mode='before')
   @classmethod
   def validate_username(cls, value):
-    if not match(r"^[a-zA-Z0-9_]+$", value):
+    if not match(USERNAME_ALPHANUMERIC_REGEX, value):
       raise ValueError("Username must be alphanumeric or contain underscores")
     return value
 
   @field_validator('password')
   @classmethod
   def validate_password(cls, value):
-    if not match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", value):
+    if not match(PASSWORD_STRONG_WITH_SPECIAL_CHARS_REGEX, value):
       raise ValueError("Password must contain at least one uppercase, one lowercase, one number, and one special character")
     return value
 
