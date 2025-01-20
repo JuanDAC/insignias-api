860c2c1fd19d0ca51963df631d2f4d84f20f6d43
JuanDAC <juanavila12.0308@gmail.com>
2025-01-20 17:03:38 -0500

    :fire: Remove extra tabulation

--- app/core/use_cases/user/get_user_level.py
+++ app/core/use_cases/user/get_user_level.py
@@ -3,5 +3,5 @@ from sqlmodel import Session, select
 
 
 def get_user_level(db: Session, user_id: int) -> User | None:
-    user= db.exec(select(User).filter(User.user_id == user_id)).first()
-    return User.model_validate(user) 
+  user= db.exec(select(User).filter(User.user_id == user_id)).first()
+  return User.model_validate(user) 
